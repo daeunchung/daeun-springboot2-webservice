1dd401ca39dee2544cf741f88411c7c67f082eb4
daeunchung <jul12230103@gmail.com>
2022-03-24 01:11:24 +0900

    add oauth properties file and gitignore it

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -41,7 +41,7 @@ public class PostsService {
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id)
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
         );
         return new PostsResponseDto(entity);
     }
