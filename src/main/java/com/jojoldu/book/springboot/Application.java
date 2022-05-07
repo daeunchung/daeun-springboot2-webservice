68c46463d6c7832ed9479df0db86df92034aa99a
daeunchung <jul12230103@gmail.com>
2022-05-07 20:30:53 +0900

    Ch5.7 기존 테스트에 시큐리티 적용하기

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+// @EnableJpaAuditing
+// JPA Auditing 활성화 => src/main/java/com/jojoldu/book/springboot/config/JpaConfig.java로 분리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,23 +1,32 @@
 package com.jojoldu.book.springboot.web;
 
 import static org.hamcrest.Matchers.is; // 요거 자동 임포트 안되서 애먹었음
+
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")  // Chapter5.7 기존 테스트에 시큐리티 적용하기 에서 목유저
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -27,6 +36,8 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+
+    @WithMockUser(roles = "USER")  // Chapter5.7 기존 테스트에 시큐리티 적용하기 에서 목유저
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
